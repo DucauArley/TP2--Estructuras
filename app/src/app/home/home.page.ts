0250525b816dd6f74187bd5783513167aacd8442
DucauArley <arleyducau@gmail.com>
2020-03-10 16:28:38 -0300

    app

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -7,6 +7,61 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public abierto9:boolean = false;
+  public abiertoCa:boolean = false;
+  public abiertoOx:boolean = false;
+  public abiertoDng:boolean = false;
+  public activos:boolean = false;
+
   constructor() {}
+
+  abrir9()
+  {
+    if(this.abierto9 == true)
+    {
+      this.abierto9 = false;
+    }
+    else
+    {
+      this.abierto9 = true;
+    }
+  }
+
+  abrirCa()
+  {
+    if(this.abiertoCa == true)
+    {
+      this.abiertoCa = false;
+    }
+    else
+    {
+      this.abiertoCa = true;
+    }
+  }
+
+  abrirOx()
+  {
+    if(this.abiertoOx == true)
+    {
+      this.abiertoOx = false;
+    }
+    else
+    {
+      this.abiertoOx = true;
+    }
+  }
+
+  abrirDng()
+  {
+    if(this.abiertoDng == true)
+    {
+      this.abiertoDng = false;
+    }
+    else
+    {
+      this.abiertoDng = true;
+    }
+  }
+
 
 }
